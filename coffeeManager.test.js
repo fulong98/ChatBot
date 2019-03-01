50db7fd90284279c67ed92da61c834a26eebfc9b
fulong98 <fulong98@hotmail.com>
2019-03-01 20:19:20 +0800

    unit test

---
+++ coffeeManager.test.js
@@ -0,0 +1,30 @@
+const CoffeeManger = requires(coffeeManager.js)
+describe('Coffee Manager', () => {
+    let coffeeManager;
+    beforeEach(() =>
+    {
+        coffeeManager = new CoffeeManager();
+        order= ['kopi','kopiC'];
+    })
+    it('it should be able to list current order', () => {
+        const coffeeManager = new CoffeeManager();
+        expect (coffeeManager.getCurrentOrders()).toEqual([]);
+    })
+});
+
+it("can receive an order",() =>{
+    const coffeeManager = new CoffeeManager();
+    coffeeManager.receiveOrder(order);
+    expect (coffeeManager.getCurrentOrders()).toEqual([order]);
+})
+
+class CoffeeManager {
+    constructor (){this.orders = []}
+    getCurrentOrders(order){
+        return this.orders;
+    }
+    receiveOrder(orders){
+        return this.orders.push(orders);
+    }
+
+}
