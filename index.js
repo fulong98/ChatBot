c7dd3ce3f88c470a89df9c534ed16c63659c9693
fulong98 <fulong98@hotmail.com>
2019-03-08 09:56:40 +0800

    added URL , Port

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 console.log("Running chatbot...");
 const Telegraf = require('telegraf');
+const URL = process.env.URL || "https://chatbotxxxasd.herokuapp.com/" ;
+const PORT = process.env.PORT || 3000;
+const API_KEY = "678358866:AAH6Nx1Nv6u-LKByxN8XMsSOJAHUW5Z-Zro";
 
-const API_KEY = "678358866:AAH6Nx1Nv6u-LKByxN8XMsSOJAHUW5Z-Zro"
-
-
+bot.use(Telegraf.log());
+bot.help(({rpely}) => reply("Hello I'm the Telegram Bot"));
 const bot = new Telegraf(API_KEY);
 
 // concept of js function
@@ -18,7 +20,7 @@ const bot = new Telegraf(API_KEY);
 // }
 
 // const greet2=name => "Hello" +name;
-
+bot.start(ctx => ctx.reply("Hello"));
 const helpHandler = context => context.reply("Hello");
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.help(helpHandler);
